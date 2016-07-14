6e7da651a622d37785d3b08b8b8ee1f713d13efc
Wu Zhangjin <wuzhangjin@gmail.com>
2016-07-14 15:58:36 +0800

    Add cross toolchains
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

---
+++ mips-toolchain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Install mips toolchain from emdebian.org
+#
+
+sudo bash -c 'echo deb http://www.emdebian.org/debian/ stable main > /etc/apt/sources.list.d/emdebian.list'
+sudo apt-get install -y emdebian-archive-keyring
+sudo apt-get -y update
+# pushd misc
+# wget -c http://archive.ubuntu.com/ubuntu/pool/universe/g/gmp4/libgmp3c2_4.3.2+dfsg-2ubuntu1_amd64.deb
+# popd
+sudo dpkg -i misc/libgmp3c2_4.3.2+dfsg-2ubuntu1_amd64.deb
+sudo apt-get -y --force-yes install gcc-4.3-mipsel-linux-gnu
