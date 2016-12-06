3a60f807349b8c59cfed89c889c2d89649677cdf
Wu Zhangjin <wuzhangjin@gmail.com>
2016-12-07 02:24:59 +0800

    jvm: Automate the installation
    ref: https://hub.docker.com/r/flurdy/oracle-java6/~/dockerfile/
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- jvm.sh
+++ jvm.sh
@@ -5,6 +5,7 @@
 
 version=6
 
+echo oracle-java6-installer shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get -y update
 sudo apt-get install -y --force-yes oracle-java${version}-installer
